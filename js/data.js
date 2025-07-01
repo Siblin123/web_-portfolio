9b03179c56d8991d570d5d1f96419f1b1ec19897
Siblin123 <wlgh7546@naver.com>
2025-07-01 14:56:40 +0900

    테스트

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 let portfolo_data = [
   {
+    work_thub_ : "img/KYWA.png",
     people :"1",
     title_: "KYWA 리뉴얼",
     date_:"5일",
@@ -7,9 +8,12 @@ let portfolo_data = [
     part:"100%",
     img_: "img/KYWA_FULL.png",
     href: "https://siblin123.github.io/KYWA/",
+    Demo_:"https://siblin123.github.io/KYWA/",
+    Github_:"https://github.com/Siblin123/KYWA",
     responsive:"제작"
   },
   {
+    work_thub_ : "img/lotte world.png",
     people :"3",
     title_: "롯대 아쿠아리움",
     date_:"5일",
@@ -17,9 +21,12 @@ let portfolo_data = [
     part:"헤더,메인 비주얼 <br>전시생물,티켓 예매",
     img_: "img/lotte world FULL.jpg",
     href: "https://siblin123.github.io/aquarium/",
+    Demo_:"https://siblin123.github.io/aquarium/",
+    Github_:"https://github.com/Siblin123/aquarium",
     responsive:"제작"
   },
   {
+    work_thub_ : "img/Music.png",
     people :"1",
     title_: "대관령 음악제",
     date_:"5일",
@@ -27,6 +34,8 @@ let portfolo_data = [
     part:"100%",
     img_: "img/Daegwallyeong.png",
     href: "https://siblin123.github.io/Daegwallyeong-Music-Festival/",
+    Demo_:"https://siblin123.github.io/Daegwallyeong-Music-Festival/",
+    Github_:"https://github.com/Siblin123/Daegwallyeong-Music-Festival",
     responsive:"제작"
   },
 ];
--- js/js.js
+++ js/js.js
@@ -1,4 +1,5 @@
 //===================변수========================
+let body = document.querySelector("body");
 let all_Category_deco = document.querySelectorAll(".category-deco");
 let card_details = document.querySelectorAll(".details");
 let View_details = document.querySelector(".View-details");
@@ -9,6 +10,7 @@ let side_bar_Mobile = document.querySelector(".Mobile");
 let fa_xmark = document.querySelectorAll(".fa-xmark");//x버튼
 let details = document.querySelectorAll(".details");//카드의 상세보기 버튼
 let info_warps_mobile_inner = document.querySelector(".info-warps-mobile-inner");
+
 
 //====================카테고리 =========================================
 all_Category_deco.forEach(Category => {
@@ -82,10 +84,17 @@ function closeDetails() {
   side_bar_Mobile.style.display = "none";
   View_details.classList.remove("active");
   overlay.classList.remove("active");
+
+  //기존 화면 스크롤 가능
+  document.body.style.overflow = 'auto';
 }
 
 function openDetails() {
   let windowWidth = window.innerWidth;
+
+
+  //기존 화면 스크롤 막기
+  document.body.style.overflow = 'hidden';
 
   if (windowWidth > 500) {
     side_bar_Pc.style.display = "flex";
@@ -122,8 +131,9 @@ details.forEach(detail => {
         document.querySelector(".work-period_M p").innerText = data[i].date_;
         document.querySelector(".responsive_M p").innerText = data[i].responsive;
         document.querySelector(".use_program_M p").innerText = data[i].use_program;
-        document.querySelector(".part_M p").innerHTML = data[i].part;
-        
+        document.querySelector(".view-git-deco a").href = data[i].Github_;
+        document.querySelector(".view-Demo-deco a").href = data[i].Demo_;
+
         console.log("동일한게 있음");
         break;
       }
@@ -134,4 +144,15 @@ details.forEach(detail => {
   });
 });
 
-//============================= 상세 보기 데이터 연결 모바일===================================
+//============================= 상세보기 켜졌을때 밖 부분 누르면 상세보기 꺼짐 ===================================
+
+overlay.addEventListener("click",(e)=>{
+  console.log("밖 배경 클릭");
+  if (View_details.classList.contains('active')) {
+    // 바깥 영역 클릭 시만 닫히게
+    if (e.target === e.currentTarget) {
+      closeDetails();
+    }
+  }
+
+})
